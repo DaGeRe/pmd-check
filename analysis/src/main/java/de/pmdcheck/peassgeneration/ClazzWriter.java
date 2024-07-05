e4d76f5bef60af02d8fb0f95b961518037927042
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2024-07-05 18:38:09 +0200

    Update to Peass 0.3.10

--- analysis/src/main/java/de/pmdcheck/peassgeneration/ClazzWriter.java
+++ analysis/src/main/java/de/pmdcheck/peassgeneration/ClazzWriter.java
@@ -19,7 +19,7 @@ import com.github.javaparser.ast.stmt.BlockStmt;
 import com.github.javaparser.ast.stmt.ReturnStmt;
 import com.github.javaparser.ast.type.ReferenceType;
 
-import de.dagere.peass.dependency.changesreading.JavaParserProvider;
+import de.dagere.nodeDiffDetector.utils.JavaParserProvider;
 import de.dagere.peass.testtransformation.ParseUtil;
 
 public class ClazzWriter {
